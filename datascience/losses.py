bc3cc758da909ccd1e2eb18e7f146cbd804db1e1
Miron Samokhvalov <mironsamokhvalov@gmail.com>
2025-10-30 18:43:17 +0300

    modified losses calculations

--- datascience/losses.py
+++ datascience/losses.py
@@ -2,27 +2,31 @@ import pandas as pd
 import sqlite3
 import numpy as np
 
-def losses():
+def sanitize_column_std(df: pd.DataFrame,col: str):
+    std = df[col].std()
+    return df[(df[col]>df[col].mean()-3*std)&(df[col]<df[col].mean()+3*std)]
+def losses_in_roubles():
     with sqlite3.connect('database.db') as conn:
         restr = 'WHERE telegram_username NOT IN ("irsamo","mironsam1405","vvitalieva","ya_lukyanova")'
         df = pd.read_sql_query(f"SELECT * FROM results {restr}",conn)
-
+    df = sanitize_column_std(sanitize_column_std(sanitize_column_std(df,"average_ti"),"person_cost"),"team_size")
     avg_ti = df[df["estimated_losses"].notna()]["average_ti"].mean()
     df1,df2 = df[df["company_id"].notna()],df[df["company_id"].isna()]
     avg_ti1 = df1[df1["estimated_losses"].notna()]["average_ti"].mean()
     avg_ti2 = df2[df2["estimated_losses"].notna()]["average_ti"].mean()
-    r = f"""```
---- Потери у всех: ---
+    r = f"""--- Потери у всех: ---
 Потери в рублях: {df["estimated_losses"].dropna(inplace=False).mean():.2f}
+Потери в рублях на человека: {df["person_cost"].dropna(inplace=False).mean():.2f}
 Потери в процентах: {100-avg_ti*10:.2f}%
 Средний ИМК (кто указывал стоимость): {avg_ti:.2f}/10
 --- Потери в командах: ---
 Потери в рублях: {df1["estimated_losses"].dropna(inplace=False).mean():.2f}
-Потери в рублях на человека: {df1["estimated_losses"].dropna(inplace=False).mean():.2f}
+Потери в рублях на человека: {df1["person_cost"].dropna(inplace=False).mean():.2f}
 Потери в процентах: {100-avg_ti1*10:.2f}%
 Средний ИМК (кто указывал стоимость): {avg_ti1:.2f}/10
 --- Потери индивидуально: --- 
 Потери в рублях: {df2["estimated_losses"].dropna(inplace=False).mean():.2f}
+Потери в рублях на человека: {df2["person_cost"].dropna(inplace=False).mean():.2f}
 Потери в процентах: {100-avg_ti2*10:.2f}%
-Средний ИМК (кто указывал стоимость): {avg_ti2:.2f}/10```"""
+Средний ИМК (кто указывал стоимость): {avg_ti2:.2f}/10"""
     return r
